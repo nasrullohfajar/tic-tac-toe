63d331eaf19f87377b0356728233fae3bbf8dd7e
nasrullohfajar <nasrullohfajar10@gmail.com>
2025-03-28 09:26:56 +0700

    fix: move player into component

---
+++ src/components/Player.jsx
@@ -0,0 +1,14 @@
+function Player({ name, symbol }) {
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">{name}</span>
+        <span className="player-symbol">{symbol}</span>
+      </span>
+
+      <button>Edit</button>
+    </li>
+  );
+}
+
+export default Player;

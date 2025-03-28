e0365501e9a8f7de83b984ffc939f5b76ed00577
nasrullohfajar <nasrullohfajar10@gmail.com>
2025-03-28 10:00:52 +0700

    feat: change player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,33 @@
-function Player({ name, symbol }) {
+import { useState } from "react";
+
+function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  }
+
+  let EditablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    EditablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange}/>
+    );
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {EditablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
 
-      <button>Edit</button>
+      <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }

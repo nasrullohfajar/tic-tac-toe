c0386f2c4aba3c2ed4b3841f7276813e6f05e94c
nasrullohfajar <nasrullohfajar10@gmail.com>
2025-04-04 09:20:17 +0700

    feat: add handle dynamic select square

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import Player from "./components/Player";
 import Gameboard from "./components/Gameboard";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare () {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
+  }
+ 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-            <Player initialName="Player 1" symbol="X"/>
-            <Player initialName="Player 2" symbol="O"/>
+        <ol id="players" className="highlight-player">
+            <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+            <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <Gameboard/>
+        <Gameboard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
     </main>
   )

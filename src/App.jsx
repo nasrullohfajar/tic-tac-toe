fb16c268d6756e34f818c4e141f15908291df526
nasrullohfajar <nasrullohfajar10@gmail.com>
2025-04-04 11:21:04 +0700

    feat: add dynamic winner name

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,13 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2",
+  });
 
   const activePlayer = deriveActivePlayer(gameTurns);
-  let gameBoard = [...initialGameBoard.map(array => [...array])];
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -49,7 +53,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -71,6 +75,14 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers, [symbol]: newName
+      }
+    })
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -79,11 +91,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,31 @@
 import { useState } from "react";
 
-function Player({ initialName, symbol, isActive }) {
+function Player({ initialName, symbol, isActive, onChangeName }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEdit = () => {
     setIsEditing((editing) => !editing);
+
+    if(isEditing){
+      onChangeName(symbol, playerName)
+    } 
   };
 
   const handleChange = (event) => {
     setPlayerName(event.target.value);
-  }
+  };
 
   let EditablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
     EditablePlayerName = (
-      <input type="text" required value={playerName} onChange={handleChange}/>
+      <input type="text" required value={playerName} onChange={handleChange} />
     );
   }
 
   return (
-    <li className={isActive ? 'active' : undefined}>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {EditablePlayerName}
         <span className="player-symbol">{symbol}</span>
